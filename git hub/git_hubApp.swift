b15991dbb4f05924e7973a4f2df3b96c0bb7da2b
Victoria <victoriaalee4@gmail.com>
2023-07-27 13:12:30 -0400

    Initial Commit

---
+++ git hub/git_hubApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_hubApp.swift
+//  git hub
+//
+//  Created by Scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct git_hubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
